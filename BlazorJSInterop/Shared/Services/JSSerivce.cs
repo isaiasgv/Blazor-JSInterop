7246e99ba2a5070d2b6b2ad6d70dc785d7521c6d
Isaias Garcia <idgv27@gmail.com>
2019-07-16 16:49:19 -0400

    Blazor JSInterop sample application

---
+++ BlazorJSInterop/Shared/Services/JSSerivce.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorJSInterop.Shared.Services
+{
+    public class JSSerivce
+    {
+        private readonly IJSRuntime _jsRuntime;
+        public JSSerivce(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public Task<string> SetText(string text)
+        {
+            return _jsRuntime.InvokeAsync<string>(
+            "SetText",
+            text);
+        }
+    }
+}
---
+++ BlazorJSInterop/wwwroot/myJsInteropFunctions.js
@@ -0,0 +1,15 @@
+﻿window.Tools = {
+    SetText:  function (newText, elemntRef) {
+            console.log(newText);
+            elemntRef.innerHTML = newText;
+            //document.getElementById("title").innerHTML = newText;
+        return newText;
+    },
+    csharpMethod: function (){
+    DotNet.invokeMethodAsync('BlazorJSInterop', 'ReturnArrayAsync')
+        .then(data => {
+            data.push("Blazor");
+            console.log(data);
+        });
+}
+};
